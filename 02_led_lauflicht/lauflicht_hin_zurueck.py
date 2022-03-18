fb5476066bf119728e171105001d3d8f29a8dc53
Martin Müllenhaupt <mm+git@netlair.de>
2022-03-18 17:18:49 +0100

    Lauflicht hin und zurück

--- 02_led_lauflicht/lauflicht_hin_zurueck.py
+++ 02_led_lauflicht/lauflicht_hin_zurueck.py
@@ -4,17 +4,22 @@ from time import sleep
 
 pin = Pin(4, Pin.OUT)
 np = NeoPixel(pin, 10)
-black = (0,0,0)
-color=(20,20,0)
-
+color_off = (0,0,0)
+color_on = (20,20,0)
+ 
+ledCount = len(np)
 iLed = 0
 
-for i in range(2*len(np)):
-    np[iLed] = black
-    iLed += 1
-    if iLed > len(np):
-        iLed = 0
-    np[iLed] = color
-    np.write()
-    time.sleep(0.1)
-    
+while True:
+    for i in range(0, 2*ledCount):
+        np[iLed] = color_off
+
+        iLed = i
+        if iLed >= ledCount:
+            iLed = 2*ledCount - i - 1
+            
+        print(iLed)
+        
+        np[iLed] = color_on
+        np.write()
+        sleep(0.1)
--- README.md
+++ README.md
@@ -94,3 +94,32 @@ while True:
         np.write()
         sleep(0.1)
 ```
+
+## Lauflicht hin und zurück
+```python
+from machine import Pin
+from neopixel import NeoPixel
+from time import sleep
+
+pin = Pin(4, Pin.OUT)
+np = NeoPixel(pin, 10)
+color_off = (0,0,0)
+color_on = (20,20,0)
+ 
+ledCount = len(np)
+iLed = 0
+
+while True:
+    for i in range(0, 2*ledCount):
+        np[iLed] = color_off
+
+        iLed = i
+        if iLed >= ledCount:
+            iLed = 2*ledCount - i - 1
+            
+        print(iLed)
+        
+        np[iLed] = color_on
+        np.write()
+        sleep(0.1)
+```
